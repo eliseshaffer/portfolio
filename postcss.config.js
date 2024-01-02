3809ff6c1d3ad68afb30bca5ffda2cd583f0a0c5
Elise Shaffer <elise@eliseshaffer.com>
2024-01-02 15:44:35 -0700

    Remove postcss-color-mod-function

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: {
     'postcss-mixins': {},
-    'postcss-color-mod-function': {
-      // Uncomment the following to import CSS variables for use in `color-mod`:
-      // importFrom: "frontend/styles/variables.css"
-    },
-    'postcss-flexbugs-fixes': {},
     'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009'
